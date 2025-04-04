c40b5cd6b8f93ad2b9a985b972c57a7e396a0655
Ibtisam <ibtisamadil273@gmail.com>
2025-04-04 15:11:21 +0200

    Setup Cinemachine Camera

---
+++ Hadal Zone/Assets/Scripts/SubmarineController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class SubmarineController : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+    private Rigidbody2D rb;
+    private float moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        moveInput = 0f;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveInput = -1f; 
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            moveInput = 1f;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+    }
+}
